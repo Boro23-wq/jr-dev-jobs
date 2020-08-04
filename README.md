d5620ffc252d8877393fb2e2f6feeb8362d976a2
Boro23 <404slimboysavage@gmail.com>
2020-08-04 13:46:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Jr-Dev-Jobs Listings Page.
+
+-> React front end w/ Hooks <br>
+-> Simple Node-Express API <br>
+-> Cron worker to fetch data <br>
+-> Simple filtering algorithm <br>
+-> Redis + node-redis <br>
+-> Deployment (NGINX on DigitalOcean droplet) <br>
---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+//components
+
+import Jobs from './Jobs';
+
+const mockJobs = [
+  {
+    title: 'SWE 1',
+    company: 'Google',
+  },
+  {
+    title: 'SWE 2',
+    company: 'Apple',
+  },
+  {
+    title: 'SWE 1',
+    company: 'Facebook',
+  },
+];
+
+function App() {
+  return (
+    <div className='App'>
+      <Jobs jobs={mockJobs} />
+    </div>
+  );
+}
+
+export default App;
